53698c70e242095afa827dbc88fc8553fb98bba9
NaeLike เนไลก์ <87599521+Naelike@users.noreply.github.com>
2023-03-07 10:49:05 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php 
+  // array data
+  $results = array();
+  $results = array(
+    '0' => array(
+      'ON' => '1',
+      'Name' => 'Bame a',
+    ),
+    '2' => array(
+      'ON' => '2',
+      'Name' => 'Bame B',
+    ),
+  );
+  // header
+  header("Content-type: text/xlsx; charset=UTF-8");
+  header("Content-Disposition: attachment; filename=name_file");
+  // output fopen
+  $output = fopen("php://output", "w");
+  // array_keys header
+  $header = array_keys($results[0]);
+  // fputcsv
+  fputcsv($output, $header);
+  foreach($results as $row) {
+    fputcsv($output, $row);
+  }
+  fclose($output);
+  // exit
+  exit;
+?>
